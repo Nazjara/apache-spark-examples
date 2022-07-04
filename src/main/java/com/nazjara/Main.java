63f4c55a03ad519f20adbf109ec8b03b0ceaf762
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2022-07-04 20:00:25 +0300

    Init commit

---
+++ src/main/java/com/nazjara/Main.java
@@ -0,0 +1,20 @@
+package com.nazjara;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var sparkConfig = new SparkConf()
+                .setAppName("Spark app")
+                .setMaster("local[*]");
+
+        try (var sparkContext = new JavaSparkContext(sparkConfig))
+        {
+            var rdd = sparkContext.parallelize(List.of(11.1, 12.1, 13.1, 14.1));
+        }
+    }
+}
