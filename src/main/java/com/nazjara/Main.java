ab34cb58ad8efb588108842d7516466752d9f254
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2022-07-05 19:25:24 +0300

    Add few more operations

--- src/main/java/com/nazjara/Main.java
+++ src/main/java/com/nazjara/Main.java
@@ -14,8 +14,12 @@ public class Main {
 
         try (var sparkContext = new JavaSparkContext(sparkConfig))
         {
-            var rdd = sparkContext.parallelize(List.of(11.1, 12.1, 13.1, 14.1));
-            System.out.println(rdd.reduce(Double::sum));
+            var rdd = sparkContext.parallelize(List.of(11L, 12L, 13L, 14L));
+            System.out.println(rdd.reduce(Long::sum)); //sum
+            var sqrtRdd = rdd.map(Math::sqrt); //square root
+            sqrtRdd.collect().forEach(System.out::println); //print all elements
+            System.out.println(sqrtRdd.count()); //count1
+            System.out.println(sqrtRdd.map(i -> 1L).reduce(Long::sum)); //count2
         }
     }
 }
