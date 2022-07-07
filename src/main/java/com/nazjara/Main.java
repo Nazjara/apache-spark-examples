ad0c89b05720218df873cec79cf190a8d82dee79
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2022-07-07 17:35:55 +0300

    Use Scala tuples

--- src/main/java/com/nazjara/Main.java
+++ src/main/java/com/nazjara/Main.java
@@ -2,6 +2,7 @@ package com.nazjara;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.List;
 
@@ -16,8 +17,8 @@ public class Main {
         {
             var rdd = sparkContext.parallelize(List.of(11L, 12L, 13L, 14L));
             System.out.println(rdd.reduce(Long::sum)); //sum
-            var sqrtRdd = rdd.map(Math::sqrt); //square root
-            sqrtRdd.collect().forEach(System.out::println); //print all elements
+            var sqrtRdd = rdd.map(value -> new Tuple2<>(value, Math.sqrt(value))); //square root
+            sqrtRdd.collect().forEach(tuple -> System.out.println(tuple._1() + ":" + tuple._2())); //print all elements
             System.out.println(sqrtRdd.count()); //count1
             System.out.println(sqrtRdd.map(i -> 1L).reduce(Long::sum)); //count2
         }
