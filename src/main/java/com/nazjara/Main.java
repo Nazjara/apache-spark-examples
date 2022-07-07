1279b2f79a8f821b74ee301bc87524f1d3b37fb8
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2022-07-07 18:32:40 +0300

    Use JavaPairRDD

--- src/main/java/com/nazjara/Main.java
+++ src/main/java/com/nazjara/Main.java
@@ -2,6 +2,7 @@ package com.nazjara;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.sparkproject.guava.collect.Iterables;
 import scala.Tuple2;
 
 import java.util.List;
@@ -13,14 +14,38 @@ public class Main {
                 .setAppName("Spark app")
                 .setMaster("local[*]");
 
-        try (var sparkContext = new JavaSparkContext(sparkConfig))
-        {
-            var rdd = sparkContext.parallelize(List.of(11L, 12L, 13L, 14L));
-            System.out.println(rdd.reduce(Long::sum)); //sum
-            var sqrtRdd = rdd.map(value -> new Tuple2<>(value, Math.sqrt(value))); //square root
+        try (var sparkContext = new JavaSparkContext(sparkConfig)) {
+            var rdd1 = sparkContext.parallelize(List.of(11L, 12L, 13L, 14L));
+            System.out.println(rdd1.reduce(Long::sum)); //sum
+            var sqrtRdd = rdd1.map(value -> new Tuple2<>(value, Math.sqrt(value))); //square root
             sqrtRdd.collect().forEach(tuple -> System.out.println(tuple._1() + ":" + tuple._2())); //print all elements
             System.out.println(sqrtRdd.count()); //count1
             System.out.println(sqrtRdd.map(i -> 1L).reduce(Long::sum)); //count2
+
+            // reduceByKey
+            sparkContext.parallelize(
+                    List.of("WARN: log message 1",
+                            "ERROR: log message 2",
+                            "FATAL: log message 3",
+                            "ERROR: log message 4",
+                            "WARN: log message 5"))
+                    .mapToPair(value -> new Tuple2<>(value.split(":")[0], 1L))
+                    .reduceByKey(Long::sum)
+                    .collect()
+                    .forEach(tuple -> System.out.println(tuple._1() + ":" + tuple._2()));
+
+            //groupByKey (performance issues)
+            sparkContext.parallelize(
+                            List.of("WARN: log message 1",
+                                    "ERROR: log message 2",
+                                    "FATAL: log message 3",
+                                    "ERROR: log message 4",
+                                    "WARN: log message 5"))
+                    .mapToPair(value -> new Tuple2<>(value.split(":")[0], 1L))
+                    .groupByKey()
+                    .collect()
+                    .forEach(tuple -> System.out.println(tuple._1() + ":" + Iterables.size(tuple._2())));
+
         }
     }
 }
