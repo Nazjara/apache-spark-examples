9bc6499a2938c68d48eb8c87fac6af68592ecee3
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2022-08-11 17:00:15 +0300

    Introduce model fitting parameters

--- src/main/java/com/nazjara/SparkMLExamples.java
+++ src/main/java/com/nazjara/SparkMLExamples.java
@@ -1,7 +1,11 @@
 package com.nazjara;
 
+import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.tuning.ParamGridBuilder;
+import org.apache.spark.ml.tuning.TrainValidationSplit;
 import org.apache.spark.sql.SparkSession;
 
 public class SparkMLExamples {
@@ -66,17 +70,35 @@ public class SparkMLExamples {
         transformedDataset.show();
 
         var combinedData = transformedDataset.randomSplit(new double[] {0.8, 0.2});
-        var trainingData = combinedData[0];
-        var testData = combinedData[1];
+        var trainingAndTestData = combinedData[0];
+        var holdOutData = combinedData[1];
 
-        var model = new LinearRegression().fit(trainingData);
+        var linearRegression = new LinearRegression();
+        var paramGridBuilder = new ParamGridBuilder();
+        var paramMap = paramGridBuilder
+                .addGrid(linearRegression.regParam(), new double[] {0.01, 0.1, 0.5})
+                .addGrid(linearRegression.elasticNetParam(), new double[] {0, 0.5, 1})
+                .build();
+
+        var trainValidationSplit = new TrainValidationSplit()
+                .setEstimator(linearRegression)
+                .setEvaluator(new RegressionEvaluator().setMetricName("r2"))
+                .setEstimatorParamMaps(paramMap)
+                .setTrainRatio(0.8);
+
+        var model = (LinearRegressionModel) trainValidationSplit
+                .fit(trainingAndTestData)
+                .bestModel();
 
         System.out.println("The training data r2 value (closer to 1 is better) is " + model.summary().r2() +
                 " and the RMSE (smaller is better) is " + model.summary().rootMeanSquaredError());
 
-        model.transform(testData).show();
+        model.transform(holdOutData).show();
 
-        System.out.println("The testing data r2 value (closer to 1 is better) is " + model.evaluate(testData).r2() +
-                " and the RMSE (smaller is better) is " + model.evaluate(testData).rootMeanSquaredError());
+        System.out.println("The testing data r2 value (closer to 1 is better) is " + model.evaluate(holdOutData).r2() +
+                " and the RMSE (smaller is better) is " + model.evaluate(holdOutData).rootMeanSquaredError());
+
+        System.out.println(model.coefficients() + " " + model.intercept());
+        System.out.println(model.getRegParam() + " " + model.getElasticNetParam());
     }
 }
