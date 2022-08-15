fbe3faf932000b36e552a150f06d3720d0ece034
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2022-08-15 21:03:58 +0300

    Add subscription management

--- src/main/java/com/nazjara/SparkMLExamples.java
+++ src/main/java/com/nazjara/SparkMLExamples.java
@@ -11,6 +11,7 @@ import org.apache.spark.ml.regression.LinearRegressionModel;
 import org.apache.spark.ml.tuning.ParamGridBuilder;
 import org.apache.spark.ml.tuning.TrainValidationSplit;
 import org.apache.spark.ml.tuning.TrainValidationSplitModel;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -18,6 +19,7 @@ import org.apache.spark.sql.SparkSession;
 import java.util.Arrays;
 
 import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.when;
 
 public class SparkMLExamples {
 
@@ -29,8 +31,9 @@ public class SparkMLExamples {
 
             sparkSession.sparkContext().setLogLevel("WARN");
 
-            gymRepsAnalysis(sparkSession);
-            housePriceAnalysis(sparkSession);
+//            gymRepsAnalysis(sparkSession);
+//            housePriceAnalysis(sparkSession);
+            subscriptionManagement(sparkSession);
         }
     }
 
@@ -149,6 +152,77 @@ public class SparkMLExamples {
                 Arrays.stream(testDataset.columns()).forEach(column2 ->
                         System.out.println("The correlation between " + column + " and " + column2 + " is " +
                                 dataset.stat().corr(column, column2))));
+    }
+
+    private static void subscriptionManagement(SparkSession sparkSession) {
+        var dataset = sparkSession.read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("src/main/resources/ml/part-r-00000-d55d9fed-7427-4d23-aa42-495275510f78.csv",
+                        "src/main/resources/ml/part-r-00001-d55d9fed-7427-4d23-aa42-495275510f78.csv",
+                        "src/main/resources/ml/part-r-00002-d55d9fed-7427-4d23-aa42-495275510f78.csv",
+                        "src/main/resources/ml/part-r-00003-d55d9fed-7427-4d23-aa42-495275510f78.csv");
+
+        dataset = dataset.filter("is_cancelled = false");
+        dataset = dataset.withColumn("firstSub", getNullSubstitutionFunction("firstSub"))
+                .withColumn("all_time_views", getNullSubstitutionFunction("all_time_views"))
+                .withColumn("last_month_views", getNullSubstitutionFunction("last_month_views"))
+                .withColumn("next_month_views", getNullSubstitutionFunction("next_month_views"))
+                .withColumnRenamed("next_month_views", "label");
+
+        var combinedData = dataset.randomSplit(new double[] {0.9, 0.1});
+        var trainingAndTestData = combinedData[0];
+        var holdOutData = combinedData[1];
+
+        var indexer = new StringIndexer();
+        indexer.setInputCols(new String[] {"payment_method_type", "country", "rebill_period_in_months"});
+        indexer.setOutputCols(new String[] {"payment_method_typeIndex", "countryIndex", "rebill_period_in_monthsIndex"});
+
+        var encoder = new OneHotEncoder();
+        encoder.setInputCols(new String[] {"payment_method_typeIndex", "countryIndex", "rebill_period_in_monthsIndex"});
+        encoder.setOutputCols(new String[] {"payment_method_typeVector", "countryVector", "rebill_period_in_monthsVector"});
+
+        var vectorAssembler = new VectorAssembler();
+        vectorAssembler
+                .setInputCols(new String[]{"payment_method_typeVector", "countryVector", "rebill_period_in_monthsVector",
+                        "firstSub", "age", "all_time_views", "last_month_views"})
+                .setOutputCol("features");
+
+        var linearRegression = new LinearRegression();
+        var paramGridBuilder = new ParamGridBuilder();
+        var paramMap = paramGridBuilder
+                .addGrid(linearRegression.regParam(), new double[] {0.01, 0.1, 0.3, 0.5, 0.7, 1})
+                .addGrid(linearRegression.elasticNetParam(), new double[] {0, 0.5, 1})
+                .build();
+
+        var trainValidationSplit = new TrainValidationSplit()
+                .setEstimator(linearRegression)
+                .setEvaluator(new RegressionEvaluator().setMetricName("r2"))
+                .setEstimatorParamMaps(paramMap)
+                .setTrainRatio(0.9);
+
+        var pipeline = new Pipeline();
+        pipeline.setStages(new PipelineStage[] {indexer, encoder, vectorAssembler, trainValidationSplit});
+        var pipelineModel = pipeline.fit(trainingAndTestData);
+        var model = (TrainValidationSplitModel) pipelineModel.stages()[3];
+        var linearRegressionModel = (LinearRegressionModel) model.bestModel();
+
+        var holdOutResults = pipelineModel.transform(holdOutData);
+        holdOutResults.show();
+        holdOutResults = holdOutResults.drop("prediction");
+
+        System.out.println("The training data r2 value (closer to 1 is better) is " + linearRegressionModel.summary().r2() +
+                " and the RMSE (smaller is better) is " + linearRegressionModel.summary().rootMeanSquaredError());
+
+        System.out.println("The testing data r2 value (closer to 1 is better) is " + linearRegressionModel.evaluate(holdOutResults).r2() +
+                " and the RMSE (smaller is better) is " + linearRegressionModel.evaluate(holdOutResults).rootMeanSquaredError());
+
+        System.out.println(linearRegressionModel.coefficients() + " " + linearRegressionModel.intercept());
+        System.out.println(linearRegressionModel.getRegParam() + " " + linearRegressionModel.getElasticNetParam());
+    }
+
+    private static Column getNullSubstitutionFunction(String columnName) {
+        return when(col(columnName).isNull(), 0).otherwise(col(columnName));
     }
 
 }
